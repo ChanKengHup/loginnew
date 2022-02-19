a2cef1282cf7f94e385e5e3144b945ac00587fe3
Dung <dungk51a2@gmail.com>
2022-02-19 21:02:46 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import "./App.css"
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Login from "./features/User/Login"
+import Signup from "./features/User/Signup"
+import Dashboard from "./features/User/Dashboard"
+import { PrivateRoute } from "./helpers/PrivateRoute"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route  element={Login} path="/login" />
+          <Route  element={Signup} path="/signup" />
+          {/* <PrivateRoute element={Dashboard} path="/" /> */}
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
---
+++ src/features/User/Dashboard.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { userSelector, fetchUserBytoken, clearState } from "./UserSlice"
+import { Oval }  from "react-loader-spinner"
+import { useNavigate  } from "react-router-dom"
+const Dashboard = () => {
+  const history = useNavigate ()
+  const dispatch = useDispatch()
+  const { isFetching, isError } = useSelector(userSelector)
+  useEffect(() => {
+    dispatch(fetchUserBytoken({ token: localStorage.getItem("token") }))
+  }, [])
+  const { username, email } = useSelector(userSelector)
+  useEffect(() => {
+    if (isError) {
+      dispatch(clearState())
+      history.push("/login")
+    }
+  }, [isError])
+  const onLogOut = () => {
+    localStorage.removeItem("token")
+    history.push("/login")
+  }
+  return (
+    <div className="container mx-auto">
+      {isFetching ? (
+        <Oval  color="#00BFFF" height={100} width={100} />
+      ) : (
+        <Fragment>
+          <div className="container mx-auto">
+            Welcome back <h3>{username}</h3>
+          </div>
+          <button
+            onClick={onLogOut}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Log Out
+          </button>
+        </Fragment>
+      )}
+    </div>
+  )
+}
+export default Dashboard
